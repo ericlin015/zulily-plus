d654641735eff1ea499c4b27c24e265f03cdd07d
Jerry Zhang <zhangjtxx@hotmail.com>
2015-02-05 23:37:08 -0800

    Try carousel

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,37 @@
-console.log('testing 123');
-console.log($);
-console.log(React);
-console.log($('img'));
+var stuff = [];
+stuff.push($('.clearfix')[4]);
+stuff.push($('.clearfix')[6]);
+stuff.push($('.clearfix')[7]);
+stuff.push($('.clearfix')[8]);
+stuff.push($('.clearfix')[9]);
+stuff.push($('.clearfix')[10]);
+$(stuff[1]).wrapAll('<div class="single-item" />');
+//$('</div>').insertAfter(stuff[0]);
+$('.single-item').slick();
+/*{
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+console.log(stuff);*/
