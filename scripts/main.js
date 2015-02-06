72620a1b576ff473c7d0255ce33ac2dc199e47d1
Eric Lin <tinyjingle@gmail.com>
2015-02-05 23:05:35 -0800

    Url init

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,29 @@
+/*
+    Constants
+*/
+
+// pages
+var mainPage = "http://www.zulily.com/",
+    mainPageLogo = "http://www.zulily.com/?ref=logo",
+    eventPage = "http://www.zulily.com/e/",
+    productPage = "http://www.zulily.com/p/",
+    cartPage = "https://www.zulily.com/2/cart";
+
+var checkForSubPage = function(url) {
+    if (url === mainPage || url === mainPageLogo) {
+        // to do for main page
+    } else if (url.indexOf(eventPage) === 0) {
+        // to do for event page
+    } else if (url.indexOf(productPage) === 0) {
+        // todo for product page
+    } else if (url === cartPage) {
+        // todo for cart page
+    }
+};
+
 console.log('testing 123');
 console.log($);
 console.log(React);
 console.log($('img'));
+
+// $('body').html('<h1>This is to test what is going on</h1>');
